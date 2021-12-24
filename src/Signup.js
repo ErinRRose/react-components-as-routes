ca46c5ecf5a97d52d6ca491b2c01eb8d303316ed
Erin <errose10@gmail.com>
2021-12-23 21:48:48 -0500

    Done.

---
+++ src/Signup.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Signup extends React.Component{
+    render() {
+        return <h1>Sign Up Here!</h1>;
+    }
+}
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -2,51 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
-const Navbar = () => (
-  <div>
-    <NavLink to="/" exact style={link} activeStyle={{ background: "darkblue", }}>
-      Home
-    </NavLink>
-    <NavLink to="/about" exact style={link} activeStyle={{ background: "darkblue", }}>
-      About
-    </NavLink>
-    <NavLink to="/login" exact style={link} activeStyle={{ background: "darkblue", }}>
-      Login
-    </NavLink>
-  </div>
-);
-const Home = () => <h1>Home!</h1>;
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Signup from './Signup';
+import Messages from './Messages';
+import Navbar from "./Navbar";
+
+
     
-const About = () => <h1>This is my about component!</h1>;
-    
-const Login = () => (
-  <form>
-    <div>
-      <input type="text" name="username" placeholder='Username' />
-      <label htmlFor='username'>Username</label>
-    </div>
-    <div>
-      <input type="password" name="password" placeholder="Password" />
-      <label htmlFor="password">Password</label>
-    </div>
-      <input type="submit" value="Login" />
-  </form>
-  );
+
+  
 
 ReactDOM.render(
   <Router>
     <div>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/login" component={Login} />
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="./messages" component={Messages} />
     </div>
   </Router>,
   document.getElementById('root')
